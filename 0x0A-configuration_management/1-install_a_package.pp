d1a247e006669658a060f03fae5c00875adc995a
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2024-02-24 00:59:27 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,6 @@
 # Puppet manifest to install Flask version 2.1.0 using pip3
 
 package { 'flask':
-  ensure         => installed,
-  provider       => 'pip3',
-  install_options => ['--version', '2.1.0'],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
